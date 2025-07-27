75f005fbf525f32a017231ef42018c9e10f52472
Aryaman singh <aryamansingh902@gmail.com>
2025-07-27 23:29:00 +0530

    landing page commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
+
 
 const Header = () => {
   return (
@@ -13,18 +16,43 @@ const Header = () => {
         />
         </Link>
       
-      
-      
+      <div className="flex items-center space-x-4">
+     <SignedIn>
+      <Link href={"/dashboard"} className="text-grey-600 hover:text-blue-600 flex items-center gap-2">
+      <Button variant="outline">
+        <LayoutDashboard size={18} />
+        <span className="hidden md:inline">Dashboard</span>
+  </Button>
+  </Link>
+
+    <Link href={"/transaction/create"}>
+      <Button className="flex items-center gap-2">
+        <PenBox size={18} />
+        <span className="hidden md:inline">Add Transaction</span>
+  </Button>
+  </Link>
+</SignedIn>
+
       <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+  <SignInButton forceRedirectUrl="/">
+    <Button variant="outline">Login</Button>
+  </SignInButton>
+</SignedOut>
+
+<SignedIn>
+  <UserButton
+  appearance={{
+    elements: {
+      avatarBox: "w-15 h-15", // Tailwind classes to set width and height
+    },
+  }}
+/>
+</SignedIn>
+</div>
+
       </nav>
     </div>
   );
 };
 
 export default Header;
-
